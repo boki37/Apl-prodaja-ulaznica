251ede9ed2f8dbacaacb147bb14adcd490ca007d
boki37 <bogdan.gojgic@gmail.com>
2026-02-06 17:34:59 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang= "en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device-width, initial-scale-initial">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title> Proaja ulaznica preko veb aplikacije>  </title>
+</head>
+
+<body> 
+    
+</body>
+</html>
